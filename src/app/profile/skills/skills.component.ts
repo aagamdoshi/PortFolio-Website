90531219045792f79e3771b51743c08b010749d5
Aagam Doshi <aagamdoshi30794@gmail.com>
2021-11-08 19:22:39 +0000

    Added Experience and skills

--- src/app/profile/skills/skills.component.ts
+++ src/app/profile/skills/skills.component.ts
@@ -15,16 +15,19 @@ export class SkillsComponent implements OnInit {
   // 0-25: #cccc00, 26-50: #ff5500, 51-75: #00e6e6, 76-100: #339900
   ngOnInit() {
     this.frontEndSkills = [
+      { name: 'React JS', score: "90%", color: "#339900" , iconURL: "devicon-react-original colored"},
       { name: 'Angular 6/7', score: "85%" , color: "#339900", iconURL: "devicon-angularjs-plain colored"},
       { name: 'Javascript', score: "85%" , color: "#339900", iconURL: "devicon-javascript-plain colored"},
       { name: 'Typescript', score: "70%", color: "#00e6e6", iconURL: "devicon-typescript-plain colored"},
       { name: 'Angular Material', score: "60%", color: "#00e6e6" , iconURL: "devicon-angularjs-plain colored"},
-      { name: 'Bootstrap', score: "50%", color: "#00e6e6", iconURL: "devicon-bootstrap-plain colored"},
-      { name: 'React JS', score: "20%", color: "#cccc00" , iconURL: "devicon-react-original colored"}
+      { name: 'Bootstrap', score: "50%", color: "#00e6e6", iconURL: "devicon-bootstrap-plain colored"}
     ];
     // validateforColor()
 
     this.backEndSkills = [
+      { name: 'Node.JS', score: "75%", color: "#339900" , iconURL: "devicon-nodejs-plain colored"},
+      { name: 'GraphQL', score: "75%", color: "#339900" , iconURL: "devicon-graphql-plain colored"},
+      { name: 'GoLang', score: "75%", color: "#339900" , iconURL: "devicon-go-original-wordmark colored"},
       { name: 'C#', score: "75%", color: "#339900" , iconURL: "devicon-csharp-plain colored"},
       { name: 'SQL Server', score: "75%", color: "#339900" , iconURL: "devicon-mysql-plain colored"},
       { name: '.Net MVC Framework', score: "65%", color: "#00e6e6" , iconURL: "devicon-dot-net-plain colored"},
@@ -33,14 +36,11 @@ export class SkillsComponent implements OnInit {
     ];
 
     this.cloudSkills = [
-      { name: 'Azure Active Directory', score: "85%", color: "#339900" , iconURL: "devicon-devicon-plain colored"},
-      { name: 'Azure Storage, Services, Vault', score: "85%", color: "#339900" , iconURL: "devicon-devicon-plain colored"},
-      { name: 'Azure SQL', score: "75%", color: "#339900" , iconURL: "devicon-devicon-plain colored"},
-      { name: 'OAuth', score: "65%", color: "#00e6e6" , iconURL: "devicon-devicon-plain colored"},
-      { name: 'Azure PaaS', score: "60%", color: "#00e6e6" , iconURL: "devicon-devicon-plain colored"},
+      { name: 'AWS', score: "85%", color: "#339900" , iconURL: "devicon-amazonwebservices-original colored"},
+     { name: 'Azure', score: "40%", color: "#ff5500" , iconURL: "devicon-azure-plain colored"},
       { name: 'Elastic Stack', score: "30%", color: "#ff5500" , iconURL: "devicon-devicon-plain colored"},
       { name: 'Jenkins CI/CD', score: "40%", color: "#ff5500" , iconURL: "devicon-jeet-plain colored"},
-      { name: 'Docker', score: "20%", color: "#cccc00" , iconURL: "devicon-docker-plain colored"},
+      { name: 'Docker', score: "50%", color: "#ff5500" , iconURL: "devicon-docker-plain colored"},
       { name: 'Kubernetes', score: "10%", color: "#cccc00" , iconURL: "devicon-devicon-plain colored"}
     ]
   }
